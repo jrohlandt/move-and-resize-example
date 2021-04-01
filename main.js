b9a497bcfbe657d605923f1eb9530e3225fe6888
Jeandre Rohlandt <jrohlandt@gmail.com>
2021-04-01 09:08:02 +0200

    resizing

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const el = document.querySelector('.item');
+let isResizing = false;
 
 el.addEventListener('mousedown', mouseDown);
 
 function mouseDown(e) {
+    if (isResizing) return;
     window.addEventListener('mousemove', mouseMove);
     window.addEventListener('mouseup', mouseUp);
 
@@ -18,7 +20,7 @@ function mouseDown(e) {
         el.style.left = rect.left - newX + "px";
         el.style.top = rect.top - newY + "px";
         
-        console.log(e.clientX, e.clientY,{prevX, prevY}, {newX, newY}, el.style.left, rect.left);
+        // console.log(e.clie ntX, e.clientY,{prevX, prevY}, {newX, newY}, el.style.left, rect.left);
         prevX = e.clientX;
         prevY = e.clientY;
     }
@@ -26,5 +28,61 @@ function mouseDown(e) {
     function mouseUp(e) {
         window.removeEventListener('mousemove', mouseMove);
         window.removeEventListener('mouseup', mouseUp);
+    }
+}
+
+const resizers = document.querySelectorAll('.resizer');
+let currentResizer;
+
+for(let resizer of resizers) {
+    resizer.addEventListener('mousedown', mouseDown);
+
+    function mouseDown(e) {
+        isResizing = true;
+        currentResizer = e.target;
+
+        let prevX = e.clientX;
+        let prevY = e.clientY;
+    
+        window.addEventListener('mousemove', mouseMove);
+        window.addEventListener('mouseup', mouseUp);
+    
+    
+        function mouseMove(e) {
+            const rect = el.getBoundingClientRect(); 
+
+            let newX = prevX - e.clientX;
+            let newY = prevY - e.clientY;
+
+             if (currentResizer.classList.contains("ne")) {
+                el.style.width = rect.width - newX + "px";
+                el.style.height = rect.height + newY + "px";
+                el.style.top = rect.top - newY + "px";
+            }
+            else if (currentResizer.classList.contains("nw")) {
+                el.style.width = rect.width + newX + "px";
+                el.style.height = rect.height + newY + "px";
+                el.style.top = rect.top - newY + "px";
+                el.style.left = rect.left - newX + "px";
+            } 
+            else if (currentResizer.classList.contains("se")) {
+                el.style.width = rect.width - newX + "px";
+                el.style.height = rect.height - newY + "px";
+            } 
+            else if (currentResizer.classList.contains("sw")) {
+                el.style.width = rect.width + newX + "px";
+                el.style.left = rect.left - newX + "px";
+                el.style.height = rect.height - newY + "px";
+            }
+
+            prevX = e.clientX;
+            prevY = e.clientY;
+        }
+    
+        function mouseUp(e) {
+            window.removeEventListener('mousemove', mouseMove);
+            window.removeEventListener('mouseup', mouseUp);
+            isResizing = false;
+        }
     }
 }
